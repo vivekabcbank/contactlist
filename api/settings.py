e738276b839e06ff080d809bac76bae143d53a67
vivekabcbank <vivek.athilkar@bank-abc.com>
2022-07-24 12:15:19 +0530

    procfile config

--- api/settings.py
+++ api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
